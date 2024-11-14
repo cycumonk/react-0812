65f96eba485a24e3897d39beb3779c026bdcfbe8
cycumonk <cycumonk@gmail.com>
2024-11-14 23:01:10 +0800

    fix: 1.修改log檔存放的資料夾，改判斷若沒有資料夾在根目錄新增一個logs資料夾放log 2.修改LogManager關閉的時機，改由判斷app關閉在關閉Logmanager

--- api/Program.cs
+++ api/Program.cs
@@ -4,7 +4,19 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 using NLog.Extensions.Logging;
+using System.IO;
 
+// 應用的根目錄
+var rootPath = AppContext.BaseDirectory;
+
+// 設定日誌資料夾路徑
+var logDirectory = Path.Combine(rootPath, "logs");
+
+// 檢查資料夾是否存在，若不存在則創建
+if (!Directory.Exists(logDirectory))
+{
+    Directory.CreateDirectory(logDirectory);
+}
 
 var config = new ConfigurationBuilder()
     .SetBasePath(AppContext.BaseDirectory) // 確保在不同平台上取得正確的路徑
@@ -38,6 +50,13 @@ builder.Services.AddControllers();
 
 var app = builder.Build();
 
+var lifetime = app.Services.GetRequiredService<IHostApplicationLifetime>();
+// app關閉的時候關閉LogMannger
+lifetime.ApplicationStopping.Register(() =>
+{
+    LogManager.Shutdown();
+});
+
 // Middlewares
 app.UseRouting();
 app.UseCors("AllowAllOrigins");
@@ -55,5 +74,3 @@ app.MapControllers();
 
 app.Run();
 
-//釋放Nlog使用的資源 確保應用程式正常退出 有些日誌訊息會放在記憶體緩衝區，如果沒有正確關閉Nlog會導致資料還在緩衝區來不及寫入就被關閉，導致日誌沒寫入
-LogManager.Shutdown();
